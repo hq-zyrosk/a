4e6b8c1294519b49c1406e85255be6750cc16fd8
x <x@x.x>
2025-01-26 20:23:43 +0530

    changes

--- flake/z/channel/hyprland/shading.glsl
+++ flake/z/channel/hyprland/shading.glsl
@@ -4,9 +4,9 @@ uniform sampler2D tex;
 out vec4 fragcolor;
 in vec2 v_texcoord;
 
-const float r = +1.0 * (3.0 / 3.0);
-const float g = +1.0 * (2.0 / 3.0);
-const float b = +1.0 * (1.0 / 3.0);
+const float r = +1.0 * (4.0 / 4.0);
+const float g = +1.0 * (4.0 / 4.0);
+const float b = +1.0 * (4.0 / 4.0);
 
 void main () {
   vec4 color = texture (tex, v_texcoord);
