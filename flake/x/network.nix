49e7f1532f0a1229f007f7d8e3e539829e49812f
x <x@x.x>
2025-01-28 22:20:56 +0530

    changes

--- flake/x/network.nix
+++ flake/x/network.nix
@@ -1,36 +1,36 @@
 { lib, ... }:
 {
   services = {
-    # tor = {
-    #   settings = {
-    #     DNSPort = [
-    #       {
-    #         addr = "127.0.0.1";
-    #         port = 53;
-    #       }
-    #     ];
-    #     VirtualAddrNetworkIPv4 = "10.192.0.0/10";
-    #     AutomapHostsSuffixes = [
-    #       ".onion"
-    #       ".exit"
-    #     ];
-    #     AutomapHostsOnResolve = true;
-    #   };
-    #   client = {
-    #     transparentProxy = {
-    #       enable = true;
-    #     };
-    #     socksListenAddress = {
-    #       port = 9050;
-    #       addr = "127.0.0.1";
-    #     };
-    #     dns = {
-    #       enable = true;
-    #     };
-    #     enable = true;
-    #   };
-    #   enable = true;
-    # };
+    tor = {
+      settings = {
+        DNSPort = [
+          {
+            addr = "127.0.0.1";
+            port = 53;
+          }
+        ];
+        VirtualAddrNetworkIPv4 = "10.192.0.0/10";
+        AutomapHostsSuffixes = [
+          ".onion"
+          ".exit"
+        ];
+        AutomapHostsOnResolve = true;
+      };
+      client = {
+        transparentProxy = {
+          enable = true;
+        };
+        socksListenAddress = {
+          port = 9050;
+          addr = "127.0.0.1";
+        };
+        dns = {
+          enable = true;
+        };
+        enable = true;
+      };
+      enable = true;
+    };
   };
 
   networking = {
@@ -41,6 +41,7 @@
       enable = true;
     };
     nameservers = [
+      "127.0.0.1"
       "8.8.8.8"
       "1.1.1.1"
     ];
