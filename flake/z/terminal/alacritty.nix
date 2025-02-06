cb8177a85cb49d0bdcc6ecf2baad9535162ef590
Zyrosk <hq.zyrosk@gmail.com>
2025-02-06 15:14:54 +0530

    changes

--- flake/z/terminal/alacritty.nix
+++ flake/z/terminal/alacritty.nix
@@ -5,13 +5,81 @@
         terminal = {
           shell = "nu";
         };
+        keyboard = {
+          bindings = [
+            {
+              key = "Q";
+              mods = "Control";
+              action = "ReceiveChar";
+              char = "\\\\x03";
+            }
+            {
+              key = "T";
+              mods = "Control";
+              action = "SpawnNewInstance";
+            }
+            {
+              key = "F";
+              mods = "Control";
+              action = "SearchStart";
+            }
+            {
+              key = "V";
+              mods = "Control";
+              action = "Paste";
+            }
+            {
+              key = "C";
+              mods = "Control";
+              action = "Copy";
+            }
+          ];
+        };
         font = {
           size = 10;
         };
+        cursor = {
+          style = {
+            shape = "beam";
+            blinking = "Off";
+          };
+          thickness = 1;
+        };
         colors = {
           primary = {
+            dim_foreground = "#000000";
             foreground = "#000000";
             background = "#FFEFDF";
+          };
+          normal = {
+            black = "#888888";
+            red = "#C90000";
+            green = "#007400";
+            yellow = "#666600";
+            blue = "#0000D0";
+            magenta = "#6F006F";
+            cyan = "#004444";
+            white = "#898989";
+          };
+          dim = {
+            black = "#888888";
+            red = "#C90000";
+            green = "#007400";
+            yellow = "#666600";
+            blue = "#0000D0";
+            magenta = "#6F006F";
+            cyan = "#004444";
+            white = "#898989";
+          };
+          bright = {
+            black = "#888888";
+            red = "#C90000";
+            green = "#007400";
+            yellow = "#666600";
+            blue = "#0000D0";
+            magenta = "#6F006F";
+            cyan = "#004444";
+            white = "#898989";
           };
         };
       };
