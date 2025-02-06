9ddcef9e798fc8af0465f91f66fd1b7b3ecad170
Zyrosk <hq.zyrosk@gmail.com>
2025-02-06 21:32:26 +0530

    changes

--- flake/z/utility/rofi.nix
+++ flake/z/utility/rofi.nix
@@ -2,7 +2,12 @@
 {
   programs = {
     rofi = {
+      plugins = with pkgs; [
+        rofi-bluetooth
+        rofi-calc
+      ];
       package = pkgs.rofi-wayland;
+      font = "PragmataPro Mono Liga";
       enable = true;
     };
   };
