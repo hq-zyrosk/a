8933a0e5b19dc04af68e2111c7c53d7f9860dd57
Zyrosk <hq.zyrosk@gmail.com>
2025-02-13 00:18:44 +0530

    Enhance VSCode settings with additional CodeLens and inline suggestion configurations

--- flake/z/codedit/vscode/setting/design/editor/code.nix
+++ flake/z/codedit/vscode/setting/design/editor/code.nix
@@ -1,6 +1,12 @@
 {
+  "merge-conflict.codeLens.enabled" = true;
+  "javascript.referencesCodeLens.showOnAllFunctions" = true;
+  "javascript.referencesCodeLens.enabled" = true;
+  "github.copilot.chat.generateTests.codeLens" = true;
   "editor.codeLens" = true;
   "editor.codeActions.triggerOnFocusChange" = true;
   "editor.codeActionWidget.showHeaders" = true;
   "editor.codeActionWidget.includeNearbyQuickFixes" = true;
+  "diffEditor.codeLens" = true;
+  "debug.javascript.codelens.npmScripts" = "all";
 }
--- flake/z/codedit/vscode/setting/design/editor/inline.nix
+++ flake/z/codedit/vscode/setting/design/editor/inline.nix
@@ -1,8 +1,12 @@
 {
+  "editor.inlineSuggest.syntaxHighlightingEnabled" = true;
   "editor.inlineSuggest.suppressSuggestions" = true;
   "editor.inlineSuggest.showToolbar" = "always";
   "editor.inlineSuggest.enabled" = true;
+  "editor.inlineSuggest.edits.renderSideBySide" = "never";
+  "editor.inlineSuggest.edits.codeShifting" = true;
   "editor.inlineCompletionsAccessibilityVerbose" = true;
   "editor.inlayHints.padding" = false;
+  "editor.inlayHints.maximumLength" = 0;
   "editor.inlayHints.enabled" = "offUnlessPressed";
 }
