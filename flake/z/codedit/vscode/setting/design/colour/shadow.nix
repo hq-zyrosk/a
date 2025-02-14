2aa1621a7f479cfff45a9732b239de7f3755f49b
Zyrosk <hq.zyrosk@gmail.com>
2025-02-15 00:17:39 +0530

    Add custom color settings for VSCode design elements

---
+++ flake/z/codedit/vscode/setting/design/colour/shadow.nix
@@ -0,0 +1,19 @@
+let
+  shadow = "#ADADAD3F";
+in
+{
+  "editor.inactiveSelectionBackground" = shadow;
+  "editor.rangeHighlightBackground" = shadow;
+  "editorHint.border" = shadow;
+  "inlineChat.regionHighlight" = shadow;
+  "minimap.selectionHighlight" = shadow;
+  "diffEditor.unchangedRegionShadow" = shadow;
+  "panelStickyScroll.shadow" = shadow;
+  "sideBarStickyScroll.shadow" = shadow;
+  "editorStickyScroll.shadow" = shadow;
+  "scrollbar.shadow" = shadow;
+  "inlineChat.shadow" = shadow;
+  "listFilterWidget.shadow" = shadow;
+  "widget.shadow" = shadow;
+  "editorUnnecessaryCode.opacity" = shadow;
+}
