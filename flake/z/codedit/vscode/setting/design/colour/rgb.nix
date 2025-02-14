e50f22a4f220756463869f83d24265d082ca115c
Zyrosk <hq.zyrosk@gmail.com>
2025-02-15 02:58:15 +0530

    Add custom color settings for VSCode design elements and remove unused undecided color configuration

---
+++ flake/z/codedit/vscode/setting/design/colour/rgb.nix
@@ -0,0 +1,22 @@
+{
+  "charts.yellow" = "#3E3E00FF";
+  "charts.red" = "#7F0000FF";
+  "charts.purple" = "#6F006FFF";
+  "charts.orange" = "#7F00003F";
+  "charts.green" = "#3E3E00FF";
+  "charts.blue" = "#004700FF";
+  "terminal.ansiYellow" = "#3E3E00FF";
+  "terminal.ansiWhite" = "#888888FF";
+  "terminal.ansiRed" = "#7F0000FF";
+  "terminal.ansiMagenta" = "#6F006FFF";
+  "terminal.ansiGreen" = "#3E3E00FF";
+  "terminal.ansiCyan" = "#004700FF";
+  "terminal.ansiBrightYellow" = "#3E3E00FF";
+  "terminal.ansiBrightWhite" = "#888888FF";
+  "terminal.ansiBrightRed" = "#7F0000FF";
+  "terminal.ansiBrightMagenta" = "#6F006FFF";
+  "terminal.ansiBrightGreen" = "#3E3E00FF";
+  "terminal.ansiBrightCyan" = "#004700FF";
+  "terminal.ansiBrightBlue" = "#004700FF";
+  "terminal.ansiBlue" = "#004700FF";
+}
