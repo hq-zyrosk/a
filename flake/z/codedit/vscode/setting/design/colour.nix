b0513f6470ae0aa167e26bd556b83142c25f4778
Zyrosk <hq.zyrosk@gmail.com>
2025-02-19 04:45:55 +0530

    changes

--- flake/z/codedit/vscode/setting/design/colour.nix
+++ flake/z/codedit/vscode/setting/design/colour.nix
@@ -1,23 +1,23 @@
 let
+  variant = import ./colour/condition.nix;
   unknown = import ./colour/unknown.nix;
   shadow = import ./colour/shadow.nix;
   rgb = import ./colour/rgb.nix;
   highlight = import ./colour/highlight.nix;
   hidden = import ./colour/hidden.nix;
   foreground = import ./colour/foreground.nix;
-  condition = import ./colour/condition.nix;
   border = import ./colour/border.nix;
   background = import ./colour/background.nix;
 in
 {
   "workbench.colorCustomizations" =
-    unknown
+    variant
+    // unknown
     // shadow
     // rgb
     // highlight
     // hidden
     // foreground
-    // condition
     // border
     // background
     // { };
