d0354396c4ef9caf69fc55f22fc34c7cdd31fc97
Zyrosk <hq.zyrosk@gmail.com>
2025-02-24 23:30:27 +0530

    Add default Firefox profile settings for theme customization

--- flake/z/browser/firefox.nix
+++ flake/z/browser/firefox.nix
@@ -2,6 +2,12 @@
 {
   programs = {
     firefox = {
+      profiles.default = {
+        settings = {
+          "browser.theme.content-theme" = 1;
+          "browser.theme.toolbar-theme" = 1;
+        };
+      };
       package = pkgs.firefox;
       enable = true;
     };
