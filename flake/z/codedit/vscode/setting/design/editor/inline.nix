e6529d278f5f74671dd3df1d0d1a4189e9712d56
Zyrosk <hq.zyrosk@gmail.com>
2025-02-26 00:22:43 +0530

    Add configuration to show collapsed inline suggestion edits in VSCode

--- flake/z/codedit/vscode/setting/design/editor/inline.nix
+++ flake/z/codedit/vscode/setting/design/editor/inline.nix
@@ -3,6 +3,7 @@
   "editor.inlineSuggest.suppressSuggestions" = true;
   "editor.inlineSuggest.showToolbar" = "always";
   "editor.inlineSuggest.enabled" = true;
+  "editor.inlineSuggest.edits.showCollapsed" = true;
   "editor.inlineSuggest.edits.renderSideBySide" = "never";
   "editor.inlineSuggest.edits.codeShifting" = true;
   "editor.inlineCompletionsAccessibilityVerbose" = true;
