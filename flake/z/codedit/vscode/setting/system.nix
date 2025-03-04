0d8340c8956c479d535f790bee47c557a22a1646
Zyrosk <hq.zyrosk@gmail.com>
2025-03-04 23:26:05 +0530

    Update VSCode system settings; add npm import and reorder application imports

--- flake/z/codedit/vscode/setting/system.nix
+++ flake/z/codedit/vscode/setting/system.nix
@@ -1,10 +1,11 @@
 let
-  app_eslint = import ./system/app/eslint.nix;
   video_screencast = import ./system/video/screencast.nix;
   video = import ./system/video.nix;
   sort = import ./system/sort.nix;
   scm = import ./system/scm.nix;
+  npm = import ./system/npm.nix;
   git = import ./system/git.nix;
+  app_eslint = import ./system/app/eslint.nix;
   app = import ./system/app.nix;
 in
-video_screencast // video // sort // scm // git // app_eslint // app
+video_screencast // video // sort // scm // npm // git // app_eslint // app
