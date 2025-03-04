59411d36ae0b151166a8035237a33c49db93425f
Zyrosk <hq.zyrosk@gmail.com>
2025-03-05 01:52:24 +0530

    Add Even Better TOML configuration to VSCode settings

--- flake/z/codedit/vscode/setting/source/toml.nix
+++ flake/z/codedit/vscode/setting/source/toml.nix
@@ -1,1 +1,33 @@
-{ }
+{
+  "evenBetterToml.schema.catalogs" = [
+    "https://www.schemastore.org/api/json/catalog.json"
+  ];
+  "evenBetterToml.schema.associations" = { };
+  "evenBetterToml.rules" = [ ];
+  "evenBetterToml.syntax.semanticTokens" = true;
+  "evenBetterToml.semanticTokens" = true;
+  "evenBetterToml.schema.links" = true;
+  "evenBetterToml.schema.enabled" = true;
+  "evenBetterToml.schema.cache.memoryExpiration" = 1350;
+  "evenBetterToml.schema.cache.diskExpiration" = 86400;
+  "evenBetterToml.formatter.trailingNewline" = true;
+  "evenBetterToml.formatter.reorderKeys" = true;
+  "evenBetterToml.formatter.reorderInlineTables" = true;
+  "evenBetterToml.formatter.reorderArrays" = true;
+  "evenBetterToml.formatter.inlineTableExpand" = true;
+  "evenBetterToml.formatter.indentTables" = true;
+  "evenBetterToml.formatter.indentString" = "  ";
+  "evenBetterToml.formatter.indentEntries" = true;
+  "evenBetterToml.formatter.crlf" = false;
+  "evenBetterToml.formatter.compactInlineTables" = false;
+  "evenBetterToml.formatter.compactEntries" = false;
+  "evenBetterToml.formatter.compactArrays" = false;
+  "evenBetterToml.formatter.columnWidth" = 0;
+  "evenBetterToml.formatter.arrayTrailingComma" = true;
+  "evenBetterToml.formatter.arrayAutoExpand" = true;
+  "evenBetterToml.formatter.arrayAutoCollapse" = false;
+  "evenBetterToml.formatter.allowedBlankLines" = 1;
+  "evenBetterToml.formatter.alignEntries" = true;
+  "evenBetterToml.formatter.alignComments" = true;
+  "evenBetterToml.completion.maxKeys" = 5;
+}
