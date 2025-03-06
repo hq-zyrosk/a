224b93ac9e9c13d75f1ab8796c23c80977efe953
Zyrosk <hq.zyrosk@gmail.com>
2025-03-06 17:55:39 +0530

    Add Nix configuration for unfree packages and reorganize static file structure

--- flake/z/storage/static.nix
+++ flake/z/storage/static.nix
@@ -1,12 +1,16 @@
 {
   home = {
     file = {
-      "justfile" = {
-        source = ./static/justfile;
-        enable = true;
-      };
       ".config/home-manager/home.nix" = {
         source = ./static/home.nix;
+        enable = true;
+      };
+      ".config/nixpkgs/config.nix" = {
+        source = ./static/config.nix;
+        enable = true;
+      };
+      "justfile" = {
+        source = ./static/justfile;
         enable = true;
       };
     };
