668ece16ef74f3578e9ecdc093e766b37669a522
Zyrosk <hq.zyrosk@gmail.com>
2025-03-06 21:33:51 +0530

    Enhance Firefox privacy settings: disable WebGL, enable fingerprinting resistance, and configure proxy settings

--- flake/z/browser/firefox.nix
+++ flake/z/browser/firefox.nix
@@ -2,24 +2,40 @@
 {
   programs = {
     firefox = {
-      profiles.default = {
-        settings = {
-          "browser.theme.content-theme" = 1;
-          "browser.theme.toolbar-theme" = 1;
-          "ui.systemUsesDarkTheme" = 1;
+      profiles = {
+        default = {
+          settings = {
+            "webgl.disabled" = true;
+            "ui.systemUsesDarkTheme" = 1;
+            "privacy.resistFingerprinting" = true;
+            "privacy.donottrackheader.enabled" = true;
+            "network.proxy.type" = 1;
+            "network.proxy.socks_remote_dns" = true;
+            "network.proxy.socks_port" = 9050;
+            "network.proxy.socks" = "127.0.0.1";
+            "media.peerconnection.enabled" = false;
+            "media.navigator.enabled" = false;
+            "geo.enabled" = false;
+            "dom.event.clipboardevents.enabled" = false;
+            # "browser.theme.toolbar-theme" = 1;
+            # "browser.theme.content-theme" = 1;
+            "browser.send_pings" = false;
+            "browser.safebrowsing.malware.enabled" = false;
+            "browser.safebrowsing.enabled" = false;
+          };
         };
       };
       policies = {
-        Proxy = {
-          UseProxyForDNS = true;
-          SOCKSVersion = 5;
-          SOCKSProxy = "127.0.0.1";
-          SOCKSPort = 9050;
-          Mode = "manual";
-        };
-        DNSOverHTTPS = {
-          Enabled = false;
-        };
+        # Proxy = {
+        #   UseProxyForDNS = true;
+        #   SOCKSVersion = 5;
+        #   SOCKSProxy = "127.0.0.1";
+        #   SOCKSPort = 9050;
+        #   Mode = "manual";
+        # };
+        # DNSOverHTTPS = {
+        #   Enabled = false;
+        # };
         OverridePostUpdatePage = "";
         OverrideFirstRunPage = "";
         NetworkPrediction = false;
@@ -27,22 +43,6 @@
         DisablePasswordReveal = true;
         DisableFormHistory = true;
         DisableFirefoxAccounts = true;
-      };
-      preferences = {
-        "webgl.disabled" = true;
-        "privacy.resistFingerprinting" = true;
-        "privacy.donottrackheader.enabled" = true;
-        "network.proxy.type" = 1;
-        "network.proxy.socks_remote_dns" = true;
-        "network.proxy.socks_port" = 9050;
-        "network.proxy.socks" = "127.0.0.1";
-        "media.peerconnection.enabled" = false;
-        "media.navigator.enabled" = false;
-        "geo.enabled" = false;
-        "dom.event.clipboardevents.enabled" = false;
-        "browser.send_pings" = false;
-        "browser.safebrowsing.malware.enabled" = false;
-        "browser.safebrowsing.enabled" = false;
       };
       package = pkgs.firefox;
       enable = true;
