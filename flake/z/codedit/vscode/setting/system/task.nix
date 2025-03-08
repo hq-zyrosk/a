144d4edc5eb1c66ba5a4ef50801d16287b79bd5e
Zyrosk <hq.zyrosk@gmail.com>
2025-03-09 00:37:59 +0530

    chore: update task settings for improved logging and automatic task handling

--- flake/z/codedit/vscode/setting/system/task.nix
+++ flake/z/codedit/vscode/setting/system/task.nix
@@ -1,3 +1,13 @@
 {
+  "task.verboseLogging" = false;
+  "task.slowProviderWarning" = true;
+  "task.saveBeforeRun" = "always";
+  "task.reconnection" = true;
+  "task.quickOpen.skip" = false;
+  "task.quickOpen.showAll" = false;
+  "task.quickOpen.history" = 30;
+  "task.quickOpen.detail" = true;
+  "task.problemMatchers.neverPrompt" = false;
   "task.autoDetect" = "off";
+  "task.allowAutomaticTasks" = "on";
 }
