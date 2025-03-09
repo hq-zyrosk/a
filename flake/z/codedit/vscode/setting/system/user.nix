5bb469e380208a53401e5a03ead6d879c2d9f492
Zyrosk <hq.zyrosk@gmail.com>
2025-03-09 19:34:34 +0530

    chore: update VSCode user settings for telemetry and settings synchronization

--- flake/z/codedit/vscode/setting/system/user.nix
+++ flake/z/codedit/vscode/setting/system/user.nix
@@ -1,6 +1,10 @@
 {
-  "telemetry.telemetryLevel" = "off";
   "update.showReleaseNotes" = true;
   "update.mode" = "none";
+  "telemetry.telemetryLevel" = "off";
+  "settingsSync.keybindingsPerPlatform" = true;
+  "settingsSync.ignoredSettings" = [ ];
+  "settingsSync.ignoredExtensions" = [ ];
+  "microsoft-authentication.implementation" = "msal";
   "keyboard.dispatch" = "code";
 }
