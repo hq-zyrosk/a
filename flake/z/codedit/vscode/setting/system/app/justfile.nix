4d8f3d3d41ea3e5e6d33e8b84c3171f196b38df9
Zyrosk <hq.zyrosk@gmail.com>
2025-03-13 23:31:42 +0530

    chore: add Just configuration for VSCode with terminal settings and logging

---
+++ flake/z/codedit/vscode/setting/system/app/justfile.nix
@@ -0,0 +1,7 @@
+{
+  "vscode-just.useSingleTerminal" = false;
+  "vscode-just.runInTerminal" = true;
+  "vscode-just.logLevel" = "info";
+  "vscode-just.justPath" = "just";
+  "vscode-just.formatOnSave" = false;
+}
