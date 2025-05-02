d869f688f98e3b8cf96aba6852f80cc702c783b6
Zyrosk <hq.zyrosk@gmail.com>
2025-05-02 22:04:25 +0530

    changes

---
+++ flake/z/codedit/vscode/setting/design/editor/goto.nix
@@ -0,0 +1,12 @@
+{
+  "editor.gotoLocation.multipleTypeDefinitions" = "peek";
+  "editor.gotoLocation.multipleReferences" = "peek";
+  "editor.gotoLocation.multipleImplementations" = "peek";
+  "editor.gotoLocation.multipleDefinitions" = "peek";
+  "editor.gotoLocation.multipleDeclarations" = "peek";
+  "editor.gotoLocation.alternativeTypeDefinitionCommand" = "editor.action.goToReferences";
+  "editor.gotoLocation.alternativeReferenceCommand" = "editor.action.goToReferences";
+  "editor.gotoLocation.alternativeImplementationCommand" = "editor.action.goToReferences";
+  "editor.gotoLocation.alternativeDefinitionCommand" = "editor.action.goToReferences";
+  "editor.gotoLocation.alternativeDeclarationCommand" = "editor.action.goToReferences";
+}
