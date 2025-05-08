94bfc8126354e67726870df2ea2b76b0ab103e8a
Zyrosk <hq.zyrosk@gmail.com>
2025-05-08 21:38:42 +0530

    changes

---
+++ flake/z/codedit/vscode/setting/system/app/github/copilot/chat.nix
@@ -0,0 +1,54 @@
+{
+  "github.copilot.chat.testGeneration.instructions" = [
+    {
+      "file" = ".ai-test-generation.md";
+    }
+  ];
+  "github.copilot.chat.reviewSelection.instructions" = [
+    {
+      "file" = ".ai-review-selection.md";
+    }
+  ];
+  "github.copilot.chat.pullRequestDescriptionGeneration.instructions" = [
+    {
+      "file" = ".ai-pullRequestDescriptionGeneration.md";
+    }
+  ];
+  "github.copilot.chat.commitMessageGeneration.instructions" = [
+    {
+      "file" = ".ai-commit-message-generation.md";
+    }
+  ];
+  "github.copilot.chat.codeGeneration.instructions" = [
+    {
+      "file" = ".ai-code-generation.md";
+    }
+  ];
+  "github.copilot.chat.useProjectTemplates" = true;
+  "github.copilot.chat.terminalChatLocation" = "chatView";
+  "github.copilot.chat.startDebugging.enabled" = true;
+  "github.copilot.chat.setupTests.enabled" = true;
+  "github.copilot.chat.search.semanticTextResults" = true;
+  "github.copilot.chat.search.keywordSuggestions" = true;
+  "github.copilot.chat.scopeSelection" = true;
+  "github.copilot.chat.reviewSelection.enabled" = true;
+  "github.copilot.chat.newWorkspaceCreation.enabled" = false;
+  "github.copilot.chat.localeOverride" = "en";
+  "github.copilot.chat.languageContext.typescript.enabled" = true;
+  "github.copilot.chat.languageContext.inline.typescript.enabled" = true;
+  "github.copilot.chat.languageContext.fix.typescript.enabled" = true;
+  "github.copilot.chat.followUps" = "always";
+  "github.copilot.chat.edits.temporalContext.enabled" = false;
+  "github.copilot.chat.edits.suggestRelatedFilesFromGitHistory" = true;
+  "github.copilot.chat.edits.suggestRelatedFilesForTests" = true;
+  "github.copilot.chat.edits.newNotebook.enabled" = true;
+  "github.copilot.chat.editor.temporalContext.enabled" = false;
+  "github.copilot.chat.copilotDebugCommand.enabled" = true;
+  "github.copilot.chat.completionContext.typescript.mode" = "on";
+  "github.copilot.chat.codesearch.enabled" = true;
+  "github.copilot.chat.codeGeneration.useInstructionFiles" = true;
+  "github.copilot.chat.byok.ollamaEndpoint" = "http://localhost:11434";
+  "github.copilot.chat.agent.thinkingTool" = true;
+  "github.copilot.chat.agent.runTasks" = true;
+  "github.copilot.chat.agent.autoFix" = true;
+}
