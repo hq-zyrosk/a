42a274b44a973933ba2b2b9544c6f75942602636
Zyrosk <hq.zyrosk@gmail.com>
2025-05-09 22:52:58 +0530

    changes

--- flake/z/utility/wayprompt.nix
+++ flake/z/utility/wayprompt.nix
@@ -1,6 +1,15 @@
 {
   programs = {
     wayprompt = {
+      settings = {
+        general = {
+          font-regular = "sans:size=14";
+          pin-square-amount = 32;
+        };
+        colours = {
+          background = "0xffffffaa";
+        };
+      };
       enable = true;
     };
   };
