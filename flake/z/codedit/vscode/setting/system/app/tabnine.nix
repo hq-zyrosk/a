87e3bcf84714cf85433acc4152880edcaa46b6a9
Zyrosk <hq.zyrosk@gmail.com>
2025-05-31 06:07:13 +0530

    changes

---
+++ flake/z/codedit/vscode/setting/system/app/tabnine.nix
@@ -0,0 +1,16 @@
+{
+  "tabnine.useProxySupport" = true;
+  "tabnine.snoozeDuration" = 1;
+  "tabnine.receiveBetaChannelUpdates" = true;
+  "tabnine.logLevel" = "";
+  "tabnine.logFilePath" = "";
+  "tabnine.logEngine" = false;
+  "tabnine.ignoreCertificateErrors" = false;
+  "tabnine.experimentalAutoImports" = true;
+  "tabnine.disableLineRegex" = [];
+  "tabnine.disableFileRegex" = [];
+  "tabnine.debounceMilliseconds" = 0;
+  "tabnine.completionsLoadingIndicatorEnabled" = true;
+  "tabnine.codeLensEnabled" = true;
+  "tabnine.cloudHost" = "";
+}
