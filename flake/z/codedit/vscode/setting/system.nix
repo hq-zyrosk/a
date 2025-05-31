7ee5f5a232eb122a602e86dfe7a73022040efc74
Zyrosk <hq.zyrosk@gmail.com>
2025-05-31 06:13:08 +0530

    changes

--- flake/z/codedit/vscode/setting/system.nix
+++ flake/z/codedit/vscode/setting/system.nix
@@ -13,8 +13,9 @@ let
   npm = import ./system/npm.nix;
   net_http = import ./system/net/http.nix;
   net = import ./system/net.nix;
-  app_github = import ./system/app/github.nix;
-  app_eslint = import ./system/app/eslint.nix;
+  cap_github = import ./system/cap/github.nix;
+  cap_eslint = import ./system/cap/eslint.nix;
+  app_tabnine = import ./system/app/tabnine.nix;
   app_dependi = import ./system/app/dependi.nix;
   app = import ./system/app.nix;
 in
@@ -32,7 +33,8 @@ in
   // npm
   // net_http
   // net
-  // app_github
-  // app_eslint
+  // cap_github
+  // cap_eslint
+  // app_tabnine
   // app_dependi
   // app
