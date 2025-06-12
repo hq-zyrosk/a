2ee5def80039bee4e0938e6b8c96b9b37bfcf8d7
Zyrosk <hq.zyrosk@gmail.com>
2025-06-13 00:41:43 +0530

    changes

--- flake/z/codedit/vscode/setting/design/colour/fg/cursor.nix
+++ flake/z/codedit/vscode/setting/design/colour/fg/cursor.nix
@@ -1,8 +1,6 @@
 let
-  c9 = _.rgba 1.00000 0.00000 0.00000 1.00000;
-  c5 = _.rgba 0.00000 0.00000 0.00000 1.00000;
-  # c1 = _.rgba 0.00000 0.00000 0.00000 1.00000;
-  _ = import ./../_.nix;
+  c9 = "#FF0000FF";
+  c1 = "#00000000";
 in {
   "terminalOverviewRuler.cursorForeground" = c9;
   "terminalCursor.foreground" = c9;
@@ -10,8 +8,8 @@ in {
   "editorMultiCursor.primary.foreground" = c9;
   "editorCursor.foreground" = c9;
 
-  "terminalCursor.background" = c5;
-  "editorMultiCursor.secondary.background" = c5;
-  "editorMultiCursor.primary.background" = c5;
-  "editorCursor.background" = c5;
+  "terminalCursor.background" = c1;
+  "editorMultiCursor.secondary.background" = c1;
+  "editorMultiCursor.primary.background" = c1;
+  "editorCursor.background" = c1;
 }
