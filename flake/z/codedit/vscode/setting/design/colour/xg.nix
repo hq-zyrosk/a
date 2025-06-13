12e32b37d9fb452292c767fd2667f370af0a3e4d
Zyrosk <hq.zyrosk@gmail.com>
2025-06-13 13:07:18 +0530

    changes

---
+++ flake/z/codedit/vscode/setting/design/colour/xg.nix
@@ -0,0 +1,35 @@
+let
+  c9 = "#000000FF";
+  c5 = "#FFFFFF3F";
+  c1 = "#ADADADFF";
+in {
+  "list.highlightForeground" = c9;
+  "list.focusHighlightForeground" = c9;
+  "editorSuggestWidget.highlightForeground" = c9;
+  "editorSuggestWidget.focusHighlightForeground" = c9;
+  "editorHoverWidget.highlightForeground" = c9;
+
+  "terminal.hoverHighlightBackground" = c5;
+  "tab.unfocusedInactiveBackground" = c5;
+  "peekViewResult.matchHighlightBackground" = c5;
+  "peekViewEditor.matchHighlightBackground" = c5;
+  "notebook.symbolHighlightBackground" = c5;
+  "interactiveEditor.regionHighlight" = c5;
+  "inlineChat.regionHighlight" = c5;
+  "editorUnicodeHighlight.background" = c5;
+  "editor.symbolHighlightBackground" = c5;
+  "editor.stackFrameHighlightBackground" = c5;
+  "editor.snippetTabstopHighlightBackground" = c5;
+  "editor.snippetFinalTabstopHighlightBackground" = c5;
+  "editor.lineHighlightBackground" = c5;
+  "editor.hoverHighlightBackground" = c5;
+  "editor.focusedStackFrameHighlightBackground" = c5;
+  "debugView.valueChangedHighlight" = c5;
+
+  "peekViewEditor.matchHighlightBorder" = c1;
+  "editorUnicodeHighlight.border" = c1;
+  "editor.symbolHighlightBorder" = c1;
+  "editor.snippetTabstopHighlightBorder" = c1;
+  "editor.snippetFinalTabstopHighlightBorder" = c1;
+  "editor.lineHighlightBorder" = c1;
+}
--- flake/z/codedit/vscode/setting/design/colour/xg/find.nix
+++ flake/z/codedit/vscode/setting/design/colour/xg/find.nix
@@ -3,14 +3,14 @@ let
   c5 = "#FFFFFF3F";
   c1 = "#ADADADFF";
 in {
+  "editor.findMatchHighlightForeground" = c9;
+  "minimap.findMatchHighlight" = c5;
+
   "terminal.findMatchBackground" = c9;
   "searchEditor.findMatchBackground" = c9;
   "editor.findMatchBackground" = c9;
   "terminal.findMatchHighlightBackground" = c5;
   "editor.findMatchHighlightBackground" = c5;
-
-  "editor.findMatchHighlightForeground" = c9;
-  "minimap.findMatchHighlight" = c5;
 
   "terminal.findMatchHighlightBorder" = c1;
   "editor.findMatchHighlightBorder" = c1;
